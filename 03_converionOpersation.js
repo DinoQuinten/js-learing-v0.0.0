f30afecae71266079d9d81973b7b8b47c74dab23
Prasanna Anthony <77713617+DinoQuinten@users.noreply.github.com>
2024-02-11 19:29:17 +0000

    operation and bad part of js

--- 03_converionOpersation.js
+++ 03_converionOpersation.js
@@ -18,3 +18,16 @@ console.log(BooleanLoggedIn)
 // 2=true
 // "" = false
 // "hello" = true
+
+
+/*Operation*/
+let value=9
+let negVal= -value
+console.log(negVal)
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3")  //why i hate js
+
