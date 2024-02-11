0018072757b1205ecd8516923993ded7d61d3319
Prasanna Anthony <77713617+DinoQuinten@users.noreply.github.com>
2024-02-11 18:55:14 +0000

    data types added

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 1234
+let accountEmail = "learn@google.com"
+var accountPassword = "1234"
+
+// accountId = 2 NOT ALLOWED
+
+// console.log(accountId)
+
+
+accountEmail="hc@hc.com"
+accountPassword="232"
+accountCity = "Beng"
+
+console.log(accountEmail)
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
