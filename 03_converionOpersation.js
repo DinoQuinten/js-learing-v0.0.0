7568b2630465cd70491ebb912e8b43e894f61acb
Prasanna Anthony <77713617+DinoQuinten@users.noreply.github.com>
2024-02-11 19:20:36 +0000

    conversion  of dataTypes

---
+++ 03_converionOpersation.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+
+console.log(typeof score);
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+
+
+//"33"= 33
+// "33avk"=NaN
+
+let isLoggedIn=1
+
+let BooleanLoggedIn= Boolean(isLoggedIn)
+console.log(BooleanLoggedIn)
+
+// 2=true
+// "" = false
+// "hello" = true
